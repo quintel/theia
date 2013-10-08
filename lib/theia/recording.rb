f6f98989722569367269f9be4980c2c1110a063b
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-10-08 09:48:06 +0200

    using max 24 frames on a recording in order to prevent memory overload

--- lib/theia/recording.rb
+++ lib/theia/recording.rb
@@ -1,5 +1,9 @@
 module Theia
+
+  # A recording contains up to 24 frames in consequetive order.
   class Recording
+
+    MAX_FRAMES = 24
 
     attr_reader :frames
 
@@ -7,8 +11,14 @@ module Theia
       @frames = []
     end
 
+    # Adds a frame to this recording. Pops off the first one, if it has hit
+    # the max amount of frames allowed.
     def add_frame(frame)
       @frames << frame
+
+      @frames.delete_at(0) if frames.size > MAX_FRAMES
+
+      @frames
     end
 
   end
