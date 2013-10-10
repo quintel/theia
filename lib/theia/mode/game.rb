30f0b305270a09aadae7b463eb26891d2361cb2b
Andre Medeiros <me@andremedeiros.info>
2013-10-10 15:47:52 +0200

    I guess someone forgot something...

--- lib/theia/mode/game.rb
+++ lib/theia/mode/game.rb
@@ -69,7 +69,9 @@ module Theia
           pieces: @pieces.sort
         }
 
-        File.write "#{ data_path }/state.yml", state.to_yaml
+        path = File.expand_path('../../../../data/', __FILE__)
+
+        File.write "#{ path }/state.yml", state.to_yaml
       end
 
     end
