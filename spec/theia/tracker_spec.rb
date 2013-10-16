6d2528dbd1b101ef64c15d42a880749ab3d4f9e2
Andre Medeiros <me@andremedeiros.info>
2013-10-16 16:00:03 +0200

    Fixed specs to reflect new behaviour

--- spec/theia/tracker_spec.rb
+++ spec/theia/tracker_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe Tracker do
   describe '#track' do
     let(:rect)       { Rect.new(50, 50, 150, 150) }
-    let(:occurrence) { Occurrence.new(rect, 'coal_plant', 1) }
+    let(:piece)      { Piece.find('coal_plant') }
+    let(:color)      { piece.color }
+    let(:occurrence) { Occurrence.new(rect, color, piece, 1) }
 
     before(:each) do
       @tracker = Tracker.new
@@ -16,21 +18,26 @@ describe Tracker do
       end
     end
 
-    context 'second frame' do
-      xit 'should report occurrences' do
-        second = Occurrence.new(rect, 'coal_plant', 2)
-        @tracker.track(second)
+    context 'when it hits MINIMUM_APEARENCES' do
+      it 'should report occurrences' do
+        Occurrence::MINIMUM_APPEARENCES.times do |cycle|
+          occurrence = Occurrence.new(rect, color, piece, cycle)
+          @tracker.cycle = cycle
+          @tracker.track(occurrence)
+        end
 
-        expect( @tracker.pieces ).to eq(['coal_plant'])
+        expect( @tracker.pieces ).to eq([occurrence])
       end
     end
 
     context 'in the future' do
       it 'should remove occurrences that pop up again' do
-        second = Occurrence.new(rect, 'coal_plant', 2)
+        second = Occurrence.new(rect, color, piece, 2)
+        @tracker.cycle = 2
         @tracker.track(second)
 
-        third = Occurrence.new(rect, 'electric_car', 20)
+        third = Occurrence.new(rect, color, piece, 20)
+        @tracker.cycle = 20
         @tracker.track(third)
 
         expect( @tracker.pieces ).to eq([])
