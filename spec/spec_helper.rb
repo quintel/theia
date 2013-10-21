54368f01f6aff04d096ee6ae18a0e44f4921bc37
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-10-21 14:38:23 +0200

    Deleted comments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,11 +17,6 @@ module Theia
     FileUtils.cp_r('spec/fixtures/.', Theia.data_path)
   end
 
-  # def self.silence_logger!
-    # logger.remove(Log4r::Outputter.stdout)
-  # end
 end
 
-# Make sure we start with a clean slate
 Theia.refresh_fixtures!
-# Theia.silence_logger!
